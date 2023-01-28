4a8ee617b90c835b979622e8666c522736e9c7e9
gavitsra <gavitsra@gmail.com>
2023-01-28 21:34:46 +0100

    allow teleporting leashed entities

--- src/main/java/me/gavitsra/teleporters/tasks/Teleport.java
+++ src/main/java/me/gavitsra/teleporters/tasks/Teleport.java
@@ -4,8 +4,12 @@ import me.gavitsra.teleporters.Teleporters;
 import org.bukkit.Location;
 import org.bukkit.Particle;
 import org.bukkit.Sound;
+import org.bukkit.entity.Entity;
+import org.bukkit.entity.LivingEntity;
 import org.bukkit.entity.Player;
 import org.bukkit.scheduler.BukkitRunnable;
+
+import java.util.List;
 
 public class Teleport extends BukkitRunnable {
     Player player;
@@ -19,6 +23,18 @@ public class Teleport extends BukkitRunnable {
     @Override
     public void run() {
         Teleporters.getInstance().teleporting.remove(player);
+
+        List<Entity> entities = player.getWorld().getEntities().stream().filter(
+          entity -> entity.getLocation().distance(player.getLocation()) < 100
+        ).toList();
+        for (Entity entity : entities) {
+            if (entity instanceof LivingEntity e) {
+                if (e.isLeashed() && e.getLeashHolder() == player) {
+                    e.teleport(loc);
+                }
+            }
+        }
+
         player.teleport(loc);
         player.getWorld().playSound(player.getLocation(), Sound.ENTITY_FOX_TELEPORT, 1, 1);
         player.getWorld().spawnParticle(Particle.END_ROD, player.getLocation(), 100, 0, 0, 0, .5);
